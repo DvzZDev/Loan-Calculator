ad48b4fa9c8b2334a1960614a01f94de78be8134
David <estebandavid578@gmail.com>
2024-05-21 22:36:50 +0200

    update

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,3 @@
-// Obtener referencias a los elementos del DOM
 let btn = document.getElementById("btn");
 let range1 = document.getElementById("range1");
 let range2 = document.getElementById("range2");
@@ -7,32 +6,55 @@ let totalPrestamoElement = document.getElementById("cantidad");
 let intMensualElement = document.getElementById("interes");
 let mensualidadElement = document.getElementById("mensualidad");
 let totalElement = document.getElementById("total");
+let años = document.getElementById("años");
 
-// Manejadores de eventos para los inputs
+const conversorAños = () => {
+  let totalMeses = parseFloat(mensualidadElement.value);
+  let años = Math.floor(totalMeses / 12);
+  let meses = Math.round(totalMeses % 12);
+  return `${años} años y ${meses} meses`;
+};
+
 range1.oninput = function () {
-  // Formatear el valor del primer rango y mostrarlo en el campo correspondiente
   let valorFormateado = new Intl.NumberFormat("es-ES").format(this.value);
   totalPrestamoElement.value = valorFormateado;
 };
 
+totalPrestamoElement.oninput = function () {
+  let valorSinFormato = this.value.replace(/\./g, "");
+  if (valorSinFormato > range1.max) {
+    range1.value = range1.max;
+  } else {
+    range1.value = valorSinFormato;
+  }
+  let valorFormateado = new Intl.NumberFormat("es-ES").format(valorSinFormato);
+  this.value = valorFormateado;
+};
+
+mensualidadElement.oninput = function () {
+  range2.value = this.value;
+  años.innerHTML = conversorAños();
+};
+
 range2.oninput = function () {
-  // Actualizar el valor de las mensualidades con el valor del segundo rango
   mensualidadElement.value = this.value;
+  años.innerHTML = conversorAños(mensualidadElement.value);
+};
+
+intMensualElement.oninput = function () {
+  let valorSinComas = this.value.replace(/,/g, ".");
+  range3.value = valorSinComas;
 };
 
 range3.oninput = function () {
-  // Actualizar el valor del interés mensual con el valor del tercer rango
   intMensualElement.value = this.value;
 };
 
-// Manejador de eventos para el botón
 btn.addEventListener("click", () => {
-  // Obtener y limpiar los valores de los inputs
   let totalPrestamo = parseInt(totalPrestamoElement.value.replace(/\./g, ""));
-  let intMensual = parseFloat(intMensualElement.value);
+  let intMensual = parseFloat(intMensualElement.value.replace(/,/g, "."));
   let mensualidad = parseInt(mensualidadElement.value);
 
-  // Función para calcular el préstamo
   const Prestamo = (totalPrestamo, intMensual, mensualidad) => {
     const interesMensual = intMensual / 100 / 12;
     const meses = mensualidad;
@@ -40,7 +62,6 @@ btn.addEventListener("click", () => {
     return (totalPrestamo * interesMensual * factorial) / (factorial - 1);
   };
 
-  // Calcular y mostrar el total del préstamo
   totalElement.innerHTML = `${Prestamo(totalPrestamo, intMensual, mensualidad).toFixed(2)}€`;
 
   document.getElementById("container-mensualidad").classList.add("expand");

55f450993f1d8d51f1a27c86499b6ce187f9030c
David <estebandavid578@gmail.com>
2024-05-21 06:00:27 +0200

    Update

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,50 @@
-const Prestamo = (totalPrestamo, interesAnual, años) => {
-  const interesMenusual = interesAnual / 100 / 12;
-  const cantidadPagos = años * 12;
-  const factorial = Math.pow(1 + interesMenusual, cantidadPagos);
-  return (cuotaMensual = (totalPrestamo * interesMenusual * factorial) / (factorial - 1));
+// Obtener referencias a los elementos del DOM
+let btn = document.getElementById("btn");
+let range1 = document.getElementById("range1");
+let range2 = document.getElementById("range2");
+let range3 = document.getElementById("range3");
+let totalPrestamoElement = document.getElementById("cantidad");
+let intMensualElement = document.getElementById("interes");
+let mensualidadElement = document.getElementById("mensualidad");
+let totalElement = document.getElementById("total");
+
+// Manejadores de eventos para los inputs
+range1.oninput = function () {
+  // Formatear el valor del primer rango y mostrarlo en el campo correspondiente
+  let valorFormateado = new Intl.NumberFormat("es-ES").format(this.value);
+  totalPrestamoElement.value = valorFormateado;
 };
-const Cuota = Prestamo(70000, 2.4, 20);
-console.log(`Cuota mensual de ${Cuota.toFixed(2)} €`);
+
+range2.oninput = function () {
+  // Actualizar el valor de las mensualidades con el valor del segundo rango
+  mensualidadElement.value = this.value;
+};
+
+range3.oninput = function () {
+  // Actualizar el valor del interés mensual con el valor del tercer rango
+  intMensualElement.value = this.value;
+};
+
+// Manejador de eventos para el botón
+btn.addEventListener("click", () => {
+  // Obtener y limpiar los valores de los inputs
+  let totalPrestamo = parseInt(totalPrestamoElement.value.replace(/\./g, ""));
+  let intMensual = parseFloat(intMensualElement.value);
+  let mensualidad = parseInt(mensualidadElement.value);
+
+  // Función para calcular el préstamo
+  const Prestamo = (totalPrestamo, intMensual, mensualidad) => {
+    const interesMensual = intMensual / 100 / 12;
+    const meses = mensualidad;
+    const factorial = Math.pow(1 + interesMensual, meses);
+    return (totalPrestamo * interesMensual * factorial) / (factorial - 1);
+  };
+
+  // Calcular y mostrar el total del préstamo
+  totalElement.innerHTML = `${Prestamo(totalPrestamo, intMensual, mensualidad).toFixed(2)}€`;
+
+  document.getElementById("container-mensualidad").classList.add("expand");
+  totalElement.classList.remove("hiden");
+
+  totalElement.classList.add("number");
+});
